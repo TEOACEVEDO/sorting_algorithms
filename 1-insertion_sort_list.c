efaf8f05864d0f5cedd553423e7a21792bf2b095
Edwin-9603 <egc4611@gmail.com>
2022-06-20 14:41:53 -0500

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,16 +7,35 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t **node_tmp = NULL;
+	listint_t *node_tmp = NULL, *tmp = NULL, *Prev = NULL;
 
-	node_tmp = list;
+	node_tmp = (*list)->next;
 
-	while(*list)
+	while(node_tmp)
 	{
-		if((*node_tmp)->n > (*node_tmp)->next->n)
-			(*node_tmp)->prev = (*node_tmp)->next->next;
-			(*node_tmp)->next = ((*node_tmp)->next->next)->prev;
+		Prev = node_tmp->prev;
+		tmp = node_tmp->next;
 
+		while(Prev && node_tmp->n < Prev->n)
+		{
+			if(Prev->prev)
+				(Prev)->prev->next = node_tmp;
 
+			(Prev)->next = node_tmp->next;
+			node_tmp->next = (Prev);
+			node_tmp->prev = (Prev)->prev;
+			(Prev)->prev = node_tmp;
+
+			if((Prev)->next)
+				(Prev)->next->prev = (Prev);
+
+			if(!node_tmp->prev)
+				(*list) = node_tmp;
+
+			print_list(*list);
+			Prev = node_tmp->prev;		
+		}
+			
+		node_tmp = tmp;
 	}
 }
